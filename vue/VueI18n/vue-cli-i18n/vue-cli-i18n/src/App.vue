acf3fdf60f748b9c8472d927622254aa9840e9a5
jiahuali <lijh@getui.com>
2019-06-28 14:58:28 +0800

    国际化框架VueI18n学习

--- vue/VueI18n/vue-cli-i18n/vue-cli-i18n/src/App.vue
+++ vue/VueI18n/vue-cli-i18n/vue-cli-i18n/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
@@ -18,9 +23,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.test-container {
-    font-size: 18px;
-    color: black;
 }
 </style>
---
+++ vue/VueI18n/vue-cli-i18n/vue-cli-i18n/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<i18n>
+  {
+    "en": {
+    "hello": "hello world!"
+  },
+  "ja": {
+    "hello": "こんにちは、世界！"
+  }
+  }
+</i18n>
+<template>
+  <div id="helloworld">
+    <label for="locale">locale</label>
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`lang${i}`" :value="lang">{{lang}}</option>
+    </select>
+    <p>message: {{$t('hello')}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'helloworld',
+  data() {
+    return {
+      langs: ['en', 'ja']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.helloworld >>> p {
+  color: #42b883;
+}
+</style>
