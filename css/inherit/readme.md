e63c51fa3e21e250a9a65e5fdc2800c209cb1c0a
jiahuali <lijh@getui.com>
2019-07-05 21:46:44 +0800

    vue文档学习

---
+++ css/inherit/readme.md
@@ -0,0 +1,3 @@
+# css可继承属性(常用)
+- 文本相关：color;font-family;font-size;font-style;font-weight;font;letter-spacing;line-height;text-align;text-indent;word-spacing;
+- 列表相关：list-style-position;list-style-image;list-style-type;list-style;
---
+++ vue/vue-doc/cookbook/svg-editable/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <p>
+      <!-- 你可以通过 prop 传递更小的 `width` 和 `height` -->
+      <icon-base width="12" height="12" icon-name="write">
+        <icon-write/>
+      </icon-base>
+      <!-- 或者你可以使用默认值，即 18 -->
+      <icon-base icon-name="write">
+        <icon-write/>
+      </icon-base>
+      <!-- 或者让它更大一些 :) -->
+      <icon-base width="30" height="30" icon-name="write">
+        <icon-write/>
+      </icon-base>
+    </p>
+  </div>
+</template>
+
+<script>
+import IconBase from './components/icon-base.vue';
+
+export default {
+  name: 'app',
+  components: {
+    IconBase
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
