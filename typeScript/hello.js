e4c04e6e86dad586e9636e90848696b1a8948bac
jiahuali <lijh@getui.com>
2019-09-18 21:15:39 +0800

    看看ts

--- typeScript/hello.js
+++ typeScript/hello.js
@@ -1,5 +1,4 @@
-function greeter(person) {
-    return "hello " + person.firstName + " " + person.lastName;
-}
-var user = { firstName: 'lee', lastName: 'jiahua' };
-document.body.innerHTML = greeter(user);
+var test;
+test = 'serven';
+test = 7;
+console.log(test);
