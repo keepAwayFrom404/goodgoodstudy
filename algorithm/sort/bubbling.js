c5ce83b9ed11603bc5b95a760ebd0f2ca6c7eadd
kirtios <lijh@getui.com>
2019-10-08 22:01:22 +0800

    排序算法bug

--- algorithm/sort/bubbling.js
+++ algorithm/sort/bubbling.js
@@ -1,10 +1,11 @@
 function exchangePosition(arr, x, y) {
   [arr[x], arr[y]] = [arr[y], arr[x]];
 }
-const arr = [11, 7, 5, 6, 87, 65];
+const arr = [12, 21, 3, 2, 4, 5, 1];
+
 function bubblingSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0, len = arr.length - 1 - i; j < len; j++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0, len = arr.length - i - 1; j < len; j++) {
       if (arr[j] > arr[j + 1]) {
         exchangePosition(arr, j, j + 1);
       }
@@ -12,3 +13,4 @@ function bubblingSort(arr) {
   }
   return arr;
 }
+console.log(bubblingSort(arr))
--- algorithm/sort/chooseSort.js
+++ algorithm/sort/chooseSort.js
@@ -1,15 +1,16 @@
 const arr = [3, 2, 4, 5, 1];
+
 function chooseSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let maxIndex = 0;
-    let len = arr.length - 1 - i;
-    for (let j = 0; j < len; j++) {
+    let len = arr.length - i;
+    for (let j = 1; j < len; j++) {
       if (arr[j] > arr[maxIndex]) {
         maxIndex = j;
       }
     }
-    console.log(arr[maxIndex], arr[len], i, arr);
-    [arr[maxIndex], arr[len]] = [arr[len], arr[maxIndex]];
+    [arr[maxIndex], arr[len - 1]] = [arr[len - 1], arr[maxIndex]];
+
   }
   return arr;
 }
