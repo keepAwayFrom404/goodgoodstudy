a5ef67e760ab9cf95dcc1d697a67fd274cc0de32
kirtios <lijh@getui.com>
2020-02-23 18:31:55 +0800

    fix: 整理项目文件

--- 1.js
+++ 1.js
@@ -1,52 +1,26 @@
-const TO_CONVERT = {
-  a_b: 123,
-  c_d: {
-    f_g_h: "asd",
-    j_k: [
-      123,
-      "123",
-      {
-        l_q: "12313"
-      },
-      [1, 2, 3]
-    ]
-  },
-  r_t: ["a", "b", "c"]
-};
-
-/**
- * @desc key 转为驼峰
- * @param [Array | Object] val 输入值
- * @retruns {Array | Object} 转换之后的数据源
- */
-function camelCase(val) {
-  if (Object.prototype.toString.call(val) === "[object Array]") {
-    let tempArr = [];
-    for (let arrVal of val) {
-      if (typeof arrVal !== "object") {
-        tempArr.push(arrVal);
-      } else {
-        tempArr.push(camelCase(arrVal));
+var obj = {
+  a: 'li',
+  b: 'jiahua'
+}
+Object.defineProperty(obj, Symbol.iterator, {
+  enumerable: false,
+  writable: false,
+  configurable: true,
+  value: function () {
+    var o = this
+    var idx = 0
+    var kys = Object.keys(obj)
+    return {
+      next: function () {
+        return {
+          value: o[kys[idx++]],
+          done: (idx > kys.length)
+        }
       }
     }
-    return tempArr;
-  } else if (Object.prototype.toString.call(val) === "[object Object]") {
-    let tempObj = {};
-    for (let key in val) {
-      let temp = keyToCamelCase(key);
-
-      if (typeof val[key] !== "object") {
-        tempObj[temp] = val[key];
-      } else {
-        tempObj[temp] = camelCase(val[key]);
-      }
-    }
-    return tempObj;
-  } else {
-    return val;
   }
-}
-function keyToCamelCase(key) {
-  return key.replace(/(^|_)(\w)/g, (m, $1, $2) => $2.toUpperCase());
-}
-console.log(camelCase(TO_CONVERT));
+})
+var it = obj[Symbol.iterator]()
+it.next()
+it.next()
+console.log(it.next())
--- debugger/index.js
+++
@@ -1,5 +0,0 @@
-const arr = [1, 2, 3, 4, 5, 6]
-for (let i = 0; i < arr.length; i++) {
-  const element = arr[i]
-  console.log(element);
-}
